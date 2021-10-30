5bf0789c4d6fd4ec40029423bb818c9feafad20a
diego325 <80598034+diego325@users.noreply.github.com>
2021-10-30 13:55:09 -0300

    Add files via upload

---
+++ src/help.js
@@ -0,0 +1,37 @@
+const help = (prefix) => {
+return`
+╔┬━━━━━━━━━━━┈ ❄︎
+││    𝗽𝗿𝗶𝘃𝗮𝘁𝗲 𝗯𝗼𝘁       
+││⇢ᴅᴏɴᴏ: 𝚜𝚔𝚒𝚕𝚕𝚎𝚛
+││⇢ɴᴜᴍᴇʀᴏ: *wa.me/5521997854480*
+││⇢ɪɴsᴛᴀ:@Chef_trem
+││⇢ᴘɪx:EM BREVE PONHO
+││⇢ᴘʀᴇғɪx: ⌈${prefix}⌋
+││⇢𝚒𝚗𝚏𝚘 𝚌𝚘𝚟𝚒𝚍-19: ⌈${prefix}covid19⌋
+││⇢𝚌𝚊𝚜𝚘 𝚎𝚗𝚌𝚘𝚗𝚝𝚛𝚎 𝚎𝚛𝚛𝚘𝚜: ⌈${prefix}bugreport⌋
+│╰────────────┈ ❄︎
+ ⊢───◣𝚙𝚛𝚒𝚟𝚊𝚝𝚎◥───✯
+│
+│⇨ *${prefix}menufig*
+│⚠️ menu de criação de stickers
+│⇨ *${prefix}plays*
+│⚠️ plays de musica
+│Downloads de audios do YT
+│⇨ *${prefix}jogos*
+│⚠️ comandos de jogos 
+│para zoar com amigos.
+│⇨ *${prefix}menuadm* 
+│ ⚠️ administração de grupos
+│⇨ *${prefix}skiller*
+│ ⚠️ comandos que so o dono do bot pode usar 
+│
+╰────⟪sᴋɪʟʟᴇʀ⟫────┈✯
+©     _*Copyright by chef tr3m*_
+®     _*dominando tudo⚡*_ 
+╘────────────💫`
+
+}
+
+
+
+exports.help = help
